4f9fa212bf81b5bf0da1c309892f97a7cd252cb5
cerlo-matavel <cerlomat@gmail.com>
2021-10-20 16:23:39 +0200

    Server alterations

---
+++ Client/src/main/java/br/com/Client/Client.java
@@ -0,0 +1,8 @@
+package br.com.Client;
+
+public class Client {
+
+    public static void main(String[] args) {
+        new Initiate().initiate();
+    }
+}
---
+++ Server/src/main/java/br/com/Server/User.java
@@ -0,0 +1,16 @@
+package br.com.Server;
+
+public class User {
+
+    private String name, email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
